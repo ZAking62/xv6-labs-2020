6247f8eab619db224de2542332e5f865015daecb
zyf1116 <zhouyongfei3@huawei.com>
2024-09-05 14:47:31 +0800

    pingpong

--- user/pingpong.c
+++ user/pingpong.c
@@ -4,34 +4,41 @@
 #define RD 0
 #define WR 1
 
-int
-main(int argc, char *argv[])
+int main(int argc, char *argv[])
 {
 
-  if(argc > 1){
+  if (argc > 1)
+  {
     fprintf(2, "Usage: pingpong\n");
     exit(1);
   }
 
-    int p2c[2];
-    int c2p[2];
-    pipe(p2c);
-    pipe(c2p);
-    printf("%d %d\n", p2c[0], p2c[1]);
-    // char buf = '0';
+  int p2c[2];
+  int c2p[2];
+  pipe(p2c);
+  pipe(c2p);
 
-    // if(fork()==0){
-    //     read(p1[1], &buf, sizeof(char));
-    //     printf("%d: received ping", getpid());
-    //     write(p2[0], &buf, sizeof(char));
-    // }
-    // else{
-    //     write(p1[0], &buf, sizeof(char));
-    //     read(p2[1], &buf, sizeof(char));
-    //     printf("%d: received ping", getpid());
-    // }
+  char buf = '0';
 
-
-
+  if (fork() == 0)
+  {
+    close(p2c[WR]);
+    // close(c2p[RD]);
+    read(p2c[RD], &buf, sizeof(char));
+    printf("%d: received ping\n", getpid());
+    write(c2p[WR], &buf, sizeof(char));
+    // close(p2c[RD]);
+    // close(c2p[WR]);
+  }
+  else
+  {
+    // close(p2c[RD]);
+    close(c2p[WR]);
+    write(p2c[WR], &buf, sizeof(char));
+    read(c2p[RD], &buf, sizeof(char));
+    printf("%d: received pong\n", getpid());
+    // close(p2c[WR]);
+    // close(c2p[RD]);
+  }
   exit(0);
 }
