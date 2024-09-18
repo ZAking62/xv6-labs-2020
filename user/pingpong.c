4621d8c62eaa3006ec0845afc005186042901eae
zyf <282711326@qq.com>
2024-09-19 00:17:33 +0800

    primes

--- user/pingpong.c
+++ user/pingpong.c
@@ -23,22 +23,22 @@ int main(int argc, char *argv[])
   if (fork() == 0)
   {
     close(p2c[WR]);
-    // close(c2p[RD]);
+    close(c2p[RD]);
     read(p2c[RD], &buf, sizeof(char));
     printf("%d: received ping\n", getpid());
     write(c2p[WR], &buf, sizeof(char));
-    // close(p2c[RD]);
-    // close(c2p[WR]);
+    close(p2c[RD]);
+    close(c2p[WR]);
   }
   else
   {
-    // close(p2c[RD]);
+    close(p2c[RD]);
     close(c2p[WR]);
     write(p2c[WR], &buf, sizeof(char));
     read(c2p[RD], &buf, sizeof(char));
     printf("%d: received pong\n", getpid());
-    // close(p2c[WR]);
-    // close(c2p[RD]);
+    close(p2c[WR]);
+     close(c2p[RD]);
   }
   exit(0);
 }
--- user/primes.c
+++ user/primes.c
@@ -1,22 +1,80 @@
 #include "kernel/types.h"
 #include "user/user.h"
 
-
 #define RD 0
 #define WR 1
 
-int main(int argc, char *argv[])
+// 每个读之前把写关掉，read读到最后就会返回0
+// 如果有fork，需要把所有写都关掉才行
+int lpipe_first_data(int lpipe[2], int *dst)
 {
-
-  if (argc > 1)
+  if (read(lpipe[RD], dst, sizeof(int)) == sizeof(int))
   {
-    fprintf(2, "Usage: primes\n");
-    exit(1);
+    printf("prime %d\n", *dst);
+    return 0;
   }
+  return -1;
+}
 
+void transmit_data(int lpipe[2], int rpipe[2], int first)
+{
+  int data;
+  // 从左管道读取数据
+  while (read(lpipe[RD], &data, sizeof(int)) == sizeof(int))
+  {
+    // 将无法整除的数据传递入右管道
+    if (data % first)
+      write(rpipe[WR], &data, sizeof(int));
+  }
+  close(lpipe[RD]);
+  close(rpipe[WR]);
+}
+
+void primes(int lpipe[2])
+{
+  int first;
+  close(lpipe[WR]);
+  if (lpipe_first_data(lpipe, &first) == 0)
+  {
+    int p[2];
+    pipe(p);
+    // 将无法整除的数据写入右管道
+    transmit_data(lpipe, p, first);
+
+    if (fork() == 0)
+    {
+      primes(p);
+    }
+    else
+    {
+      close(p[WR]);
+      wait(0);
+      close(p[RD]);
+    }
+  }
+  exit(0);
+}
+
+int main(int argc, char const *argv[])
+{
   int p[2];
   pipe(p);
 
+  for (int i = 2; i <= 35; ++i)
+  {
+    write(p[WR], &i, sizeof(int));
+  }
+  if (fork() == 0)
+  {
+    primes(p);
+  }
+  else
+  {
+    // 主动关闭，防止子进程读阻塞
+    close(p[WR]);
+    wait(0);
+    close(p[RD]);
+  }
 
   exit(0);
 }
